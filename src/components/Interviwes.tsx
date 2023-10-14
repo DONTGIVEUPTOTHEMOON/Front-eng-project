56c30d27faf47f865a9e995a4890dc45950c7a80
Tonfern <thananaeam@gmail.com>
2023-10-14 17:57:17 +0700

    arch of course && interview

--- src/components/Interviwes.tsx
+++ src/components/Interviwes.tsx
@@ -8,17 +8,12 @@ const Interviwes = () => {
       <div className={classes.sectionHeader}>
         <div>
           <div className={classes.sectionTitle}>
-            <svg width="18" height="19" viewBox="0 0 18 19" xmlns="http://www.w3.org/2000/svg">
-              <rect x="1" y="1" width="16" height="17" fill="none" stroke="currentColor" strokeWidth="2" />
-              <rect x="4" y="4" width="10" height="2" fill="currentColor" stroke="none" />
-              <rect x="4" y="8" width="8" height="2" fill="currentColor" stroke="none" />
-              <rect x="4" y="12" width="5" height="2" fill="currentColor" stroke="none" />
+            <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35">
+              <path d="m363-390 117-71 117 71-31-133 104-90-137-11-53-126-53 126-137 11 104 90-31 133ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z" />
             </svg>
             <h1>Stories from real people</h1>
           </div>
-          <p>Whtch and read storise from the Arahacademy community</p>
         </div>
-        <p className={classes.viewAll}>Explore more stories</p>
       </div>
       <div className={classes.blogContainer}>
         {INTERVIWE.map((interviwe) => {
--- src/constant/interviwe-data.ts
+++ src/constant/interviwe-data.ts
@@ -5,21 +5,21 @@ export const INTERVIWE: InterviweDTO[] = [
     thumbnailUrl:
       'https://images.unsplash.com/photo-1609151354448-c4a53450c6e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2381&q=80',
     description:
-      'To find user insight or make critical business decisions, our team relies heavily on data. We compile our little insights into this publication, The DeFi Review.',
+      '"The elders here start teaching from the basics. It makes me feel less pressured about coming to study without drawing basics."',
     user: 'Thananchanok',
     date: 'Sep 4th, 2022',
   },
   {
     thumbnailUrl:
       'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2835&q=80',
-    description: 'Is being talked about more and more as the days go by. So what is it exactly?',
+    description: '"When making a portfolio, he always comes to help look at it, help select work, and give advice."',
     user: 'Chaiwat',
     date: 'Sep 4th, 2022',
   },
   {
     thumbnailUrl:
       'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2744&q=80',
-    description: 'A walkthrough guide on how to use Hardhat to develop a smart contract with ease!',
+    description: '"A walkthrough guide on how to use Hardhat to develop a smart contract with ease!"',
     user: 'Phatanuch',
     date: 'Aug 5th, 2022',
   },
@@ -27,8 +27,17 @@ export const INTERVIWE: InterviweDTO[] = [
     thumbnailUrl:
       'https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
     description:
-      'When developing your DApp, would transaction fees be quite expensive? What do you decide between scaling, price, and security?',
+      '"From drawing skills to zero, Here the basics ars laid from the beginning. Util the end, it is very complete."',
     user: 'Wannawong',
     date: 'Sep 27th, 2022',
   },
+
+  // {
+  //   thumbnailUrl:
+  //     'https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
+  //   description:
+  //     'When developing your DApp, would transaction fees be quite expensive? What do you decide between scaling, price, and security?',
+  //   user: 'Wannawong',
+  //   date: 'Sep 27th, 2022',
+  // },
 ]
