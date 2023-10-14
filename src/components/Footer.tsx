d55c655433d4d6a0b683dc3b6116b82f85e238f0
Tonfern <thananaeam@gmail.com>
2023-10-14 22:53:58 +0700

    edit and add Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,25 +3,33 @@ import classes from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={classes.containerFooter}>
-      <div className={classes.menu}>
+      <div className={classes.cardLogo}>
         <h1>Archacdemy</h1>
-        <p>Start studying from the basics For experice within 4 months!</p>
+        <p className={classes.p}>Start studying from the basics For experice within 4 months!</p>
       </div>
 
-      <div className={classes.menu}>
+      <div className={classes.cardSocial}>
         <h1>Social Media</h1>
-        <div>
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-            IG
-          </a>
+        <div className={classes.cardlink}>
           <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-            Facebook
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+              height="50"
+              width="50"
+            />
           </a>
           <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
-            linkin
+            <img src="https://brandlogos.net/wp-content/uploads/2016/06/linkedin-logo.png" height="50" width="50" />
           </a>
           <a href="https://bonbon-living.com/" target="_blank" rel="noopener noreferrer">
-            www
+            <img src="https://1000logos.net/wp-content/uploads/2021/05/Gmail-logo.png" height="50" width="90" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+              height="50"
+              width="50"
+            />
           </a>
         </div>
       </div>
